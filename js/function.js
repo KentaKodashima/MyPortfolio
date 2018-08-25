c72c120f6c2e15023cfbef6edc788882c213d2a3
Kenta Kodashima <kentakodashima@gmail.com>
2018-08-24 18:27:36 -0700

    Modified the some UI stuff. - header area - implemented the smooth scrolling - fixed the bug that calc(% + px) doesn't work on IE 11 by adding css hacking code

--- js/function.js
+++ js/function.js
@@ -104,5 +104,16 @@ $(function () {
     $("div.navbar-collapse").removeClass('show');
     $("#nav").removeClass('isOpen');
   });
-});
 
+  // Smooth scroll
+  var headerHight = 40; // Not the navbar to cover the section title
+  $('a[href^="#"]').click(function () {
+    var speed = 400;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - headerHight;
+    $('body,html').animate({ scrollTop: position }, speed, 'swing');
+    return false;
+  });
+
+});
