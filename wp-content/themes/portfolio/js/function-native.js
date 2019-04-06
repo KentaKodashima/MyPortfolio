47b6a9f060ef45b189459d39979f3abbd91bd98f
Kenta Kodashima <kentakodashima@gmail.com>
2019-04-06 15:17:10 -0700

    Highlight the menu without jQuery.
    TODO:
     - Implement smooth scrolling without jQuery

--- wp-content/themes/portfolio/js/function-native.js
+++ wp-content/themes/portfolio/js/function-native.js
@@ -16,13 +16,11 @@ const htmlElement = document.getElementsByTagName("html")[0];
 const navElement = document.getElementById('nav')
 const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-let elementsToAnimate = getElements('.animation');
-let sections = getElements('section.section');
-let offsets = [];
-let sectionOffsetTops = [];
+const elementsToAnimate = getElements('.animation');
+const sections = getElements('section.section');
 
 /**
- * Get elements using querySelectorAll().
+ * Get HTML elements using querySelectorAll().
  * @param {string} element - String which specifies HTML element
  * */
 function getElements(element) {
@@ -42,7 +40,8 @@ function getElements(element) {
  * */
 function getOffsetTops(elements, array) {
   elements.forEach(element => {
-    array.push(element.offsetTop);
+    array.push(getOffsetTopDifference(element));
+    // array.push(element.offsetTop);
   });
 }
 
@@ -79,13 +78,13 @@ function isNavOpen() {
  * A helper function to highlight the menu to indicate which section the user is at.
  * @param {number} sectionNum - A section index
  * */
-function chengeSection(sectionNum) {
-  let navWorks = document.getElementsByClassName('nav-works');
-  let navBlog = document.getElementsByClassName('nav-blog');
-  let navSkills = document.getElementsByClassName('nav-skills');
-  let navResources = document.getElementsByClassName('nav-resources');
-  let navProfile = document.getElementsByClassName('nav-profile');
-  let navContact = document.getElementsByClassName('nav-contact');
+function changeSection(sectionNum) {
+  const navWorks = document.getElementsByClassName('nav-works')[0];
+  const navBlog = document.getElementsByClassName('nav-blog')[0];
+  const navSkills = document.getElementsByClassName('nav-skills')[0];
+  const navResources = document.getElementsByClassName('nav-resources')[0];
+  const navProfile = document.getElementsByClassName('nav-profile')[0];
+  const navContact = document.getElementsByClassName('nav-contact')[0];
   let currentSec = -1;
 
   if (sectionNum != currentSec) {
@@ -136,67 +135,94 @@ function chengeSection(sectionNum) {
   }
 }
 
-getOffsetTops(elementsToAnimate, offsets);
-getOffsetTops(sections, sectionOffsetTops);
+function smoothScrolling() {
+  const headerHeight = 40;
+  const linksInsidePage = document.querySelectorAll('a[href^="#"]');
+  // const urlHash = location.hash;
+  // if (urlHash) {
+  //   $('body,html').stop().scrollTop(0);
+  //   setTimeout(function () {
+  //     var target = $(urlHash);
+  //     var position = target.offset().top - headerHeight;
+  //     $('body,html').stop().animate({ scrollTop: position }, 500);
+  //   }, 200);
+  // };
+
+  // Links inside the same page
+  linksInsidePage.forEach(anchor => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const speed = 400;
+      const href = anchor.getAttribute('href');
+      const target = document.querySelector(href == "#" || href == "" ? 'html' : href);
+      const position = target.getBoundingClientRect().top - headerHeight;
+      bodyElement.animate({scrollTop: [position]}, speed);
+      htmlElement.animate({scrollTop: [position]}, speed);
+      // $('body,html').animate({ scrollTop: position }, speed, 'swing');
+      return false;
+    })
+  });
+}
+
+let hamburgerButton = document.getElementsByClassName('navbar-toggler')[0];
+hamburgerButton.addEventListener('click', () => {
+  navElement.classList.toggle('isOpen');
+})
+
+const navLinks = getElements('a.nav-link');
+const navbarToggler = getElements('button.navbar-toggler')[0];
+const navbarCollapse = getElements('div.navbar-collapse')[0];
+navLinks.forEach(element => {
+  element.addEventListener('click', () => {
+    navbarToggler.classList.add('collapsed');
+    navbarCollapse.classList.remove('show');
+    navElement.classList.remove('isOpen');
+  });
+});
 
 window.addEventListener('scroll', () => {
-  let ua = navigator.userAgent;
+  const ua = navigator.userAgent;
   if (ua.indexOf('iPhone') > 0 ||
     ua.indexOf('iPod') > 0 ||
     ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
 
     isNavOpen()
+  }
 
-  } else {
-    /* How many pixels did the nav move? */
-    let mainTopElement = document.getElementsByClassName('mainTop')[0];
-    let headerBottom = getOffsetTopDifference(mainTopElement);
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  /* How many pixels did the nav move? */
+  const mainTopElement = document.getElementsByClassName('mainTop')[0];
+  const headerBottom = getOffsetTopDifference(mainTopElement);
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    if (scrollTop >= headerBottom) {
-      navElement.classList.add('navScroll');
-    } else if (scrollTop <= headerBottom) {
-      navElement.classList.remove('navScroll');
-      let navWorks = document.getElementsByClassName('nav-works')[0];
-      if (navWorks.classList.contains('active')) {
-        navWorks.classList.remove('active');
-      }
-    }
-
-    // Each section's position
-    sections.forEach(function (val, i) {
-      return getOffsetTopDifference(val) - 14;
-    });
-
-    // Keyframe animations
-    elementsToAnimate.forEach(function (val) {
-      let position = getOffsetTopDifference(val); // The distance from the top to the element
-      if (scrollTop > position - windowHeight) { // The scroll position passed the element
-        val.classList.add('active'); // Add the 'active' class
-      }
-    });
-
-    // Scroll events
-    for (let i = sections.length - 1; i >= 0; i--) {
-      if (scrollTop > sections[i] - 50) {
-        chengeSection(i);
-        break;
-      }
+  if (scrollTop >= headerBottom) {
+    navElement.classList.add('navScroll');
+  } else if (scrollTop <= headerBottom) {
+    navElement.classList.remove('navScroll');
+    const navWorks = document.getElementsByClassName('nav-works')[0];
+    if (navWorks.classList.contains('active')) {
+      navWorks.classList.remove('active');
     }
   }
+
+  // Highlights menu
+  sections.forEach((val, index) => {
+    if (scrollTop > (getOffsetTopDifference(val) - 50)) {
+      changeSection(index);
+    }
+  });
+
+  // Keyframe animations
+  elementsToAnimate.forEach(val => {
+    const position = getOffsetTopDifference(val); // The distance from the top to the element
+    if (scrollTop > position - windowHeight) { // The scroll position passed the element
+      val.classList.add('active'); // Add the 'active' class
+    }
+  });
 })
 
-// $(function () {
-//   // Toggling isOpen class
-//   $('button.navbar-toggler').click(function () {
-//     $("#nav").toggleClass('isOpen');
-//   });
+// console.log(document.querySelectorAll('a[href^="#"]'))
 
-//   $('a.nav-link').click(function () {
-//     $("button.navbar-toggler").addClass('collapsed');
-//     $("div.navbar-collapse").removeClass('show');
-//     navElement.removeClass('isOpen');
-//   });
+// $(function () {
 
 //   // Smooth scroll
 //   var headerHeight = 40; // Not the navbar to cover the section title
@@ -211,7 +237,7 @@ window.addEventListener('scroll', () => {
 //       $('body,html').stop().animate({ scrollTop: position }, 500);
 //     }, 200);
 //   }
-
+//
 //   // Links inside the same page
 //   $('a[href^="#"]').click(function () {
 //     var speed = 400;
